e467e90f7157a57ee5de82f266f2fa0b4de4402f
Aryan Donga <daryan6680@proton.me>
2023-12-21 22:35:08 +0530

    update the relay code

--- new_relay.py
+++ new_relay.py
@@ -1,39 +1,13 @@
-import asyncio
 import json
+import sys
 from copy import deepcopy
 
 from discord_webhook import DiscordEmbed, AsyncDiscordWebhook
-from telethon.sync import TelegramClient, events
-from telethon.tl.types import InputChannel, MessageEntityBold, MessageEntityItalic, \
+from telethon import TelegramClient, events
+from telethon.tl.types import InputChannel
+from telethon.tl.types import MessageEntityBold, MessageEntityItalic, \
     MessageEntityStrike, MessageEntityCode, MessageEntityPre, MessageEntityMention, \
     MessageEntityMentionName, MessageEntityHashtag, MessageEntityTextUrl
-
-with open("config.json", "r") as f:
-    config = json.load(f)
-
-telegram_client = TelegramClient(**config["telegram_settings"])
-
-input_channels_entities = []
-output_channels = {}
-
-
-@telegram_client.on(events.NewMessage(chats=input_channels_entities))
-async def handle_telegram_message(event):
-    result = resolve_input_id(event.message.to_id.channel_id)
-    if result is None:
-        return
-    channel_name, output_hooks = result
-
-    message = event.message.message
-    source_message = deepcopy(message)
-    for entity in event.message.entities:
-        message = decorate_message(message, source_message, entity)
-
-    embed = DiscordEmbed(title=channel_name, description=message, color=0x00ff00)
-
-    for webhook in output_hooks:
-        webhook_reply = await send_webhook(embed, webhook)
-        print(webhook_reply.status_code)
 
 
 async def send_webhook(embed, webhook):
@@ -48,56 +22,86 @@ async def send_webhook(embed, webhook):
 
 
 def resolve_input_id(channel_id):
-    if channel_id not in config["telegram_channels"].values():
+    if channel_id not in config["telegram_channels"]:
         return None
 
     for stream in config["streams"]:
         if channel_id in stream["input_channels"]:
-            return config["telegram_channels"][str(channel_id)], stream["output_hooks"]
+            return config["telegram_channels"][channel_id], stream["output_hooks"]
 
 
 def decorate_message(current_message, source_message, entity):
-    start = entity.offset - 4
+    begin = entity.offset - 4
     end = entity.offset - 4 + entity.length
-    target_text = source_message[start:end]
+    target_text = source_message[begin:end]
+    decorated_message = current_message
 
-    decorators = {
-        MessageEntityBold: ("**", "**"),
-        MessageEntityItalic: ("*", "*"),
-        MessageEntityStrike: ("~~", "~~"),
-        MessageEntityCode: ("`", "`"),
-        MessageEntityPre: ("```", "```"),
-        MessageEntityMention: ("@", ""),
-        MessageEntityMentionName: ("@", ""),
-        MessageEntityHashtag: ("#", ""),
-        MessageEntityTextUrl: (f"[{target_text}]({entity.url})", "")
-    }
+    if isinstance(entity, MessageEntityBold):
+        decorated_message = current_message.replace(target_text, f"**{target_text}**", 1)
+    elif isinstance(entity, MessageEntityItalic):
+        decorated_message = current_message.replace(target_text, f"*{target_text}*", 1)
+    elif isinstance(entity, MessageEntityStrike):
+        decorated_message = current_message.replace(target_text, f"~~{target_text}~~", 1)
+    elif isinstance(entity, MessageEntityCode):
+        decorated_message = current_message.replace(target_text, f"`{target_text}`", 1)
+    elif isinstance(entity, MessageEntityPre):
+        decorated_message = current_message.replace(target_text, f"```{target_text}```", 1)
+    elif isinstance(entity, MessageEntityMention):
+        decorated_message = current_message.replace(target_text, f"@{target_text}", 1)
+    elif isinstance(entity, MessageEntityMentionName):
+        decorated_message = current_message.replace(target_text, f"@{target_text}", 1)
+    elif isinstance(entity, MessageEntityHashtag):
+        decorated_message = current_message.replace(target_text, f"#{target_text}", 1)
+    elif isinstance(entity, MessageEntityTextUrl):
+        decorated_message = current_message.replace(target_text, f"[{target_text}]({entity.url})", 1)
 
-    for entity_type, (prefix, suffix) in decorators.items():
-        if isinstance(entity, entity_type):
-            current_message = current_message.replace(target_text, f"{prefix}{target_text}{suffix}", 1)
-
-    return current_message
-
-
-async def fetch_telegram_channels():
-    async for d in telegram_client.iter_dialogs():
-        if d.name in config["telegram_channels"] or d.entity.id in config["telegram_channels"].values():
-            input_channels_entities.append(InputChannel(d.entity.id, d.entity.access_hash))
+    return decorated_message
 
 
-async def main():
-    await telegram_client.start()
+def start():
+    client = TelegramClient(**config["telegram_settings"])
+    client.start()
+
+    input_channels_entities = []
+
+    for d in client.iter_dialogs():
+        if (
+                d.name in config["telegram_channels"].values()
+                or d.entity.id in config["telegram_channels"]
+        ):
+            input_channels_entities.append(
+                InputChannel(d.entity.id, d.entity.access_hash)
+            )
 
     if not input_channels_entities:
-        await telegram_client.disconnect()
-        return
+        print("Could not find any input channels in the user's dialogs")
+        sys.exit(1)
 
-    await fetch_telegram_channels()
     print(f"Listening on {len(input_channels_entities)} channels")
 
-    await asyncio.gather(telegram_client.run_until_disconnected())
+    @client.on(events.NewMessage(chats=input_channels_entities))
+    async def handler(event):
+        result = resolve_input_id(str(event.message.to_id.channel_id))
+        if result is None:
+            return
+        channel_name, output_hooks = result
+
+        message = event.message.message
+        if event.message.entities is not None:
+            source_message = deepcopy(message)
+            for entity in event.message.entities:
+                message = decorate_message(message, source_message, entity)
+
+        embed = DiscordEmbed(title=channel_name, description=message, color=0x00ff00)
+
+        for webhook in output_hooks:
+            webhook_reply = await send_webhook(embed, webhook)
+            print(f"Sent message to {webhook_reply.url}")
+
+    client.run_until_disconnected()
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+start()
